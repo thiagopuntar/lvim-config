81fb62aa2ec5b5fc91200803b8c3beadf780a81e
Thiago Puntar <35710202+thiagopuntar@users.noreply.github.com>
2023-07-03 10:42:21 -0300

    add vim-surround

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,11 +1,7 @@
 lvim.plugins = {
-  {
-    "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood",
-  },
-  {
-    "github/copilot.vim"
-  },
+  { "ThePrimeagen/vim-be-good", cmd = "VimBeGood", },
+  { "github/copilot.vim" },
+  { "tpope/vim-surround" },
 }
 
 lvim.builtin.telescope.pickers = {
