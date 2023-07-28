35543a2d24e7c81bdd1a31cabb33dca864773201
Thiago Puntar <thiagopuntar@MacBook-Pro-de-Thiago.local>
2023-07-28 16:54:38 -0300

    add tmux-navigator & config telescope

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -25,3 +25,32 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.telescope.pickers = {
+  find_files = {
+    layout_config = {
+      width = 0.95,
+    },
+  },
+  git_files = {
+    layout_config = {
+      width = 0.95,
+      height = 0.95,
+    },
+    path_display = { "absolute" },
+    layout_strategy = "vertical",
+    hidden = true
+  },
+  grep_string = {
+    layout_config = {
+      width = 0.95,
+      height = 0.95,
+    }
+  },
+  live_grep = {
+    layout_config = {
+      width = 0.95,
+      height = 0.95
+    },
+    layout_strategy = "vertical",
+  },
+}
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,34 +1,6 @@
 lvim.plugins = {
-  { "ThePrimeagen/vim-be-good", cmd = "VimBeGood", },
+  { "ThePrimeagen/vim-be-good",      cmd = "VimBeGood", },
   { "github/copilot.vim" },
   { "tpope/vim-surround" },
-}
-
-lvim.builtin.telescope.pickers = {
-  find_files = {
-    layout_config = {
-      width = 0.95,
-    },
-  },
-  git_files = {
-    layout_config = {
-      width = 0.95,
-      height = 0.95,
-    },
-    path_display = { "absolute" },
-    layout_strategy = "vertical",
-    hidden = true
-  },
-  -- grep_string = {
-  --   layout_config = {
-  --     width = 0.95,
-  --     height = 0.95,
-  --   },
-  -- },
-  live_grep = {
-    layout_config = {
-      width = 0.95,
-    },
-    layout_strategy = "vertical",
-  },
+  { "christoomey/vim-tmux-navigator" },
 }
